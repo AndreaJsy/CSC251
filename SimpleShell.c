532ee405429f82c4e9cba4eca479b8d426030a63
Shiyu "Andrea" Jiang <97689754+AndreaJsy@users.noreply.github.com>
2024-02-22 18:11:08 -0500

    Update SimpleShell.c

--- SimpleShell.c
+++ SimpleShell.c
@@ -8,7 +8,7 @@
 #define MAX_ARGS 128
 #define MAX_INPUTTS 1000
 
-// Tested and worked: pwd, mkdir, rmdir, rm -rf, ls, echo
+// Tested and worked: pwd, mkdir, rmdir, rm -rf, ls, echo, exit, &, disabled ^C
 // Tested and not worked with only execvp: cd, pipe, redirection (echo "hello" > hello.txt)
 
 void ignore_control_C() {
@@ -21,16 +21,17 @@ int main(){
     char input[MAX_INPUTTS];
     int argc = 0;
     int pid;
-    int background = 0; // Flag for background execution
+    int background = 0; // Flag for background execution, set to 1 if '&' detected at command end.
 
     argv[argc] = NULL; // make sure the last element in argv is '/0' or NULL
 
-
     ignore_control_C(); //comment out for debugging purpose
+
+    printf("Hello World to terminal. To quit, enter 'exit'.\n\n");
 
     while(1){
         printf("SimpleShell-> ");
-        fflush(stdout);
+        //fflush(stdout);
 
         // Read input
         if (fgets(input, sizeof(input), stdin) == NULL) { // Error or EOF
@@ -40,12 +41,12 @@ int main(){
         // Check for newline character and replace it with null terminator
         input[strcspn(input, "\n")] = 0;
 
-        if (strcmp(input, "exit") == 0) {// if input equal to 'exit' ,exit command
+        if (strcmp(input, "exit") == 0) { // if input equal to 'exit', exit SimpleShell
             break;
         }
 
         argc = 0;
-        background = 0; // Flag for background execution
+        background = 0;
         
         char *token = strtok(input, " ");
         while (token != NULL) {
@@ -56,7 +57,7 @@ int main(){
             argv[argc++] = token;
             token = strtok(NULL, " ");
         }
-        argv[argc] = NULL; // Null-terminate argv
+        argv[argc] = NULL; // Null-terminate argv at the last char
 
         if (argc == 0) { // if input empty command, stop and return to beginning of loop
             continue;
@@ -69,9 +70,8 @@ int main(){
                 exit(EXIT_FAILURE);
             }
         } else if (pid > 0) { // Parent process
-            if (!background) {
-                int status;
-                waitpid(pid, &status, 0);
+            if (!background) { // Wait for the child process to complete if not running in the background.
+                wait(NULL);
             }
         } else { // Fork failed
             perror("fork failed");
